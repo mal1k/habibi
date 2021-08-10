fbf10bdd8d066498d9489efaa4641af115003c20
mal1k <00154@ukr.net>
2021-08-11 00:34:43 +0300

    changed structure

--- routes/web.php
+++ routes/web.php
@@ -16,50 +16,73 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('new-admin/{id}', function ($id) {
-    $user = User::findOrFail($id);
-    $status = $user->assignRole('admin');
+// Route::get('new-admin/{id}', function ($id) {
+//     $user = User::findOrFail($id);
+//     $status = $user->assignRole('admin');
 
-    return $status;
-});
+//     return $status;
+// });
 
-Route::group(['middleware' => ['role:admin']], function () {
-  // home link
-    Route::get('/home', [homeController::class, 'index'])->name('home.index');
-  // home handlers
-    Route::post('/home/store', [homeController::class, 'store'])->name('home.store');
-    Route::post('/home/update', [homeController::class, 'update'])->name('home.update');
-});
+# admin part
+  Route::group(['middleware' => ['auth', 'role:admin']], function () {
+    Route::prefix('admin')->group(function () {
+    # Links
+      // dashboard
+        Route::view('/', 'admin.dashboard');
+      // home editor link
+        Route::get('/home', [homeController::class, 'index'])->name('home.index');
+      // hookah editor link
+        // Route::get('/hookah', [homeController::class, 'index'])->name('hookah.index');
+        Route::view('/hookah', 'admin.hookah.dashboard');
+        Route::post('/example', function () {
+            return 'true';
+        })->name('test');
+      // hookah categories link
+        // Route::get('/hookah/categories', [homeController::class, 'index'])->name('home.index');
 
-
-// client part
-Route::get('/', function () {
-    $home = home::first();
-    return view('layout', compact('home'));
-});
-
-Route::get('/hookahs', function () {
-    $home = home::first();
-    return view('hookahs', compact('home'));
-})->name('menu.hookahs');
-
-Route::get('/bar', function () {
-    $home = home::first();
-    return view('bar', compact('home'));
-})->name('menu.bar');
-
-Route::get('/services', function () {
-    $home = home::first();
-    return view('services', compact('home'));
-})->name('menu.services');
-
-Route::get('/take-away', function () {
-    $home = home::first();
-    return view('takeAway', compact('home'));
-})->name('menu.takeAway');
+    # Handlers
+      // home handlers
+        Route::prefix('home')->group(function () {
+            Route::post('/store', [homeController::class, 'store'])->name('home.store');
+            Route::post('/update', [homeController::class, 'update'])->name('home.update');
+        });
+      // hookah handlers
+        Route::prefix('hookah')->group(function () {
+          Route::post('/store', [homeController::class, 'store'])->name('home.store');
+          Route::post('/update', [homeController::class, 'update'])->name('home.update');
+        });
+    });
+  });
 
 
+# client part
+    Route::get('/', function () {
+        $home = home::first();
+        return view('layout', compact('home'));
+    });
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+    Route::get('/hookahs', function () {
+        $home = home::first();
+        return view('hookahs', compact('home'));
+    })->name('menu.hookahs');
+
+    Route::get('/bar', function () {
+        $home = home::first();
+        return view('bar', compact('home'));
+    })->name('menu.bar');
+
+    Route::get('/services', function () {
+        $home = home::first();
+        return view('services', compact('home'));
+    })->name('menu.services');
+
+    Route::get('/take-away', function () {
+        $home = home::first();
+        return view('takeAway', compact('home'));
+    })->name('menu.takeAway');
+
+
+# authenticate part
+    Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
