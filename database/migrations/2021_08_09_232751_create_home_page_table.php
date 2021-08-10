7a14cedb32dd47e58e33ba597d5ac8a4faf3a280
mal1k <00154@ukr.net>
2021-08-10 03:26:34 +0300

    created migration file

---
+++ database/migrations/2021_08_09_232751_create_home_page_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHomePageTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('home_page', function (Blueprint $table) {
+            $table->id();
+
+            // logo
+            $table->string('logo');
+
+            // hookah part
+            $table->string('left_block_img');
+            $table->string('left_block_text');
+
+            // bar part
+            $table->string('upper_block_img');
+            $table->string('upper_block_text');
+
+            // services part
+            $table->string('central_block_img');
+            $table->string('central_block_text');
+
+            // with us part
+            $table->string('lower_block_img');
+            $table->string('lower_block_text');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('home_page');
+    }
+}
