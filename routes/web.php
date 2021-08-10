9f9dfdcb767e5a8263294c7b0d7c9a8e8bd8b853
mal1k <00154@ukr.net>
2021-08-10 04:18:37 +0300

    init roles

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,6 +13,17 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('new-admin/{id}', function ($id) {
+    $user = User::findOrFail($id);
+    $status = $user->assignRole('admin');
+
+    return $status;
+});
+
+Route::group(['middleware' => ['role:admin']], function () {
+    // some pages here
+});
 
 Route::get('/', function () {
     return view('welcome');
