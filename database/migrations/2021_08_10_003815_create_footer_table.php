da0ef806a3af930362fa5310ad4331dac0d43f02
mal1k <00154@ukr.net>
2021-08-10 03:41:38 +0300

    added footer migration

---
+++ database/migrations/2021_08_10_003815_create_footer_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFooterTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('footer', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('instagram')->nullable();
+            $table->integer('phone')->nullable();
+            $table->string('map')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('footer');
+    }
+}
