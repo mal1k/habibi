2ee7d7f4d13bbc307c85e2c4885425d1b4963a25
mal1k <00154@ukr.net>
2021-08-13 16:19:59 +0300

    fixed

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -8,55 +8,18 @@
 @section('content')
 <div class="header-clear-medium">
 <div id="page">
-    <div class="content mb-0 mt-3">
-        <div class="row mb-0">
-            <div class="col-6 pe-0">
-                <a class="card mx-0 mb-3 card-style default-link" data-card-height="410" href="{{ route('admin.hookah.index') }}" style="background-image:url({{ asset('/storage/' . $home->left_block_img) }})">
-                    <div class="card-bottom">
-                        <h3 class="color-white text-center mb-2">@isset ( $home->left_block_text ) {{ $home->left_block_text }} @endisset</h3>
-                    </div>
-                    @isset ( $home->left_block_text )
-                    <div class="card-overlay bg-gradient"></div>
-                    @endisset
-                </a>
-            </div>
-            <div class="col-6">
-                <a class="card mx-0 mb-2 card-style default-link" data-card-height="132"href="#" style="background-image:url({{ asset('/storage/' . $home->upper_block_img) }})">
-                    <div class="card-bottom">
-                        <h3 class="color-white text-center mb-2">@isset ( $home->upper_block_text ) {{ $home->upper_block_text }} @endisset</h3>
-                    </div>
-                    @isset ( $home->upper_block_text )
-                        <div class="card-overlay bg-gradient"></div>
-                    @endisset
-                </a>
-                <a class="card mx-0 mb-2 card-style default-link" data-card-height="131"href="#" style="background-image:url({{ asset('/storage/' . $home->central_block_img) }})">
-                    <div class="card-bottom">
-                        <h3 class="color-white text-center mb-2">@isset ( $home->central_block_text ) {{ $home->central_block_text }} @endisset</h3>
-                    </div>
-                    @isset ( $home->central_block_text )
-                        <div class="card-overlay bg-gradient"></div>
-                    @endisset
-                </a>
-                <a class="card mx-0 mb-2 card-style default-link" data-card-height="132"href="#" style="background-image:url({{ asset('/storage/' . $home->lower_block_img) }})">
-                    <div class="card-bottom">
-                        <h3 class="color-white text-center mb-2">@isset ( $home->lower_block_text ) {{ $home->lower_block_text }} @endisset</h3>
-                    </div>
-                    @isset ( $home->lower_block_text )
-                        <div class="card-overlay bg-gradient"></div>
-                    @endisset
-                </a>
-            </div>
-            <div class="col-12">
-                <a class="card mx-0 mb-2 card-style default-link" data-card-height="132"href="{{ route('menu.bar') }}" style="background-image:url({{ asset('/storage/' . $home->upper_block_img) }})">
-                    <div class="card-bottom">
-                        <h3 class="color-white text-center mb-2">Главная</h3>
-                    </div>
-                        <div class="card-overlay bg-gradient"></div>
-                </a>
+
+    <div class="card card-style">
+        <div class="content mb-0">
+            <p class="text-center mb-n1 font-12 font-600 color-highlight">Popularity in 2020</p>
+            <h1 class="text-center font-28 mb-4">Mobile OS</h1>
+            <div class="chart-container mb-4" style="width:100%; height:350px;"><div class="chartjs-size-monitor" style="position: absolute; inset: 0px; overflow: hidden; pointer-events: none; visibility: hidden; z-index: -1;"><div class="chartjs-size-monitor-expand" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;"><div style="position:absolute;width:1000000px;height:1000000px;left:0;top:0"></div></div><div class="chartjs-size-monitor-shrink" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;"><div style="position:absolute;width:200%;height:200%;left:0; top:0"></div></div></div>
+            <canvas class="chart" id="line-chart"/>
             </div>
         </div>
     </div>
 
+</div>
 </div>
 <!-- Page content ends here-->
 @endsection
