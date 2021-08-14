c8f36f33e8635c9d8e1f8fa528b86d0849c20bf9
mal1k <00154@ukr.net>
2021-08-15 01:39:18 +0300

    updated get function

--- app/Http/Controllers/tobaccoController.php
+++ app/Http/Controllers/tobaccoController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\tobacco;
 use Illuminate\Http\Request;
 
 class tobaccoController extends Controller
 {
-    //
+    public function get(Request $request)
+    {
+        $hookah = tobacco::where('id' , '=' , $request->id )->first();
+        return json_decode($hookah);
+    }
 }
