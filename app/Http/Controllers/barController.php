c4678cf894527dd434dd3264f5e45802a8e741c3
mal1k <00154@ukr.net>
2021-08-17 18:27:54 +0300

    added index func

--- app/Http/Controllers/barController.php
+++ app/Http/Controllers/barController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\bar;
 use Illuminate\Http\Request;
 
 class barController extends Controller
 {
-    //
+  // index
+    public function index()
+        {
+            $bar_query = bar::orderByDesc('id');
+            $bar = $bar_query->paginate(0);
+
+            return view('admin.bar.dashboard', compact('bar'));
+        }
 }
